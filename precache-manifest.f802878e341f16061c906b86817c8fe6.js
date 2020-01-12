b3719d10c9755f75f28414b57bc2c0a81070e056
Rafael Costa <rafaelsevla@gmail.com>
2020-01-12 19:38:11 -0300

    Updates

---
+++ precache-manifest.f802878e341f16061c906b86817c8fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49769c9b3c6e8fff0d6dee4732d1366",
+    "url": "/lemons-app/index.html"
+  },
+  {
+    "revision": "db8dafec05c45054aa95",
+    "url": "/lemons-app/static/js/2.263bdc22.chunk.js"
+  },
+  {
+    "revision": "301d10c5aba713d0bece",
+    "url": "/lemons-app/static/js/main.effea548.chunk.js"
+  },
+  {
+    "revision": "90a3a73776ba4a139d0f",
+    "url": "/lemons-app/static/js/runtime~main.9baa0466.js"
+  },
+  {
+    "revision": "5a7107927ad5f6218c8818407d791b23",
+    "url": "/lemons-app/static/media/lemonade.5a710792.png"
+  },
+  {
+    "revision": "00f899af632f5cc6212fdc762e77aceb",
+    "url": "/lemons-app/static/media/lemons.00f899af.png"
+  }
+]);
